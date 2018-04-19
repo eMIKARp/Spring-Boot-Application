a2327e7a954eef992d09122457f7578b04911ab6
Karpowicz <Emil.Karpowicz@unilever.com>
2018-04-19 22:30:33 +0200

    Add RestController that returns Hello world

---
+++ src/main/java/com/emikarp/springbootapplication/Main/Hello/HelloController.java
@@ -0,0 +1,15 @@
+
+package com.emikarp.springbootapplication.Main.Hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController 
+{
+    @RequestMapping("/hello")
+    public String sayHello()
+    {
+        return "Hello world!";
+    }
+}
